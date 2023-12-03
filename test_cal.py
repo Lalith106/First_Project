8cfc316068ab0a94d039e87e87156e4cc19dab21
lalith <cavlalithchandra2k@gmail.com>
2023-12-03 22:21:52 +0530

    Second Commit Done

--- test_cal.py
+++ test_cal.py
@@ -4,5 +4,10 @@ class TestCalculator:
 
     def test_addition(self):
         assert 6==calculator.add(2,4)
+        
     def test_subtraction(self):
         assert 2==calculator.sub(4,2)
+
+    def test_multiplication(self):
+        assert 100==calculator.multiply(10,10)
+
